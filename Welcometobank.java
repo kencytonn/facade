db4441d9ac95e6bc77843ed818484e7f2e21e5b7
kencytonn <kmutua@cytonn.com>
2015-09-15 18:25:27 +0300

    Facade repository on Design Patterns

---
+++ Welcometobank.java
@@ -0,0 +1,6 @@
+public class WelcomeToBank{
+    public WelcomeToBank() {
+        System.out.println("Welcome to Cytonn Bank");
+        System.out.println("We are happy to serve you and meet your needs\n");
+    }
+}
